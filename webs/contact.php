a121d439bc65f24e5b3a3e06b1864ca03948d4a8
Viollae-Cahaya <95465414+Viollae-Cahaya@users.noreply.github.com>
2021-12-03 21:01:11 +0700

    website

---
+++ webs/contact.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="contact.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <title>Responsive Contact us page template</title>
+
+</head>
+<body>
+    <!-- navbar -->
+    <nav class="navbar">
+            <div class="nav">
+                <img src="asset/nhslogo_auto_x2 copy.png" class="brand-logo" alt="">
+                <div class="nav-items">
+                    <div class="search">
+                        <input type="text" class="search-box" placeholder="search product">
+                        <button class="search-btn">search</button>
+                    </div>
+                    <a href="#"><img src="asset/img/cart.png" alt=""></a>
+                </div>
+            </div>
+            <ul class="links-container">
+                    <li class="link-item"><a href="index.php" class="link">home</a></li>
+                    <li class="link-item"><a href="#" class="link">profile</a></li>
+                    <li class="link-item"><a href="contactus.php" class="link">contact us</a></li>
+                    <li class="link-item"><a href="#" class="link">help</a></li>
+            </ul>
+        </nav>
+
+    <div class="container">
+        <h1>Contact Us</h1>
+        <div class="contact-items">
+            <div class="customer-care">
+                <h3>Customer Care</h3>
+                <p>Our Support team is spread all over the world to give you fast response</p>
+                <button id="cust-care"> Submit a Request</button>
+                <a href="#"> Frequently asked Questions</a>
+            </div>
+            <div class="write-us">
+            <h3>Social Media</h3>
+                <ul class="icons">
+                    <li><i class="fab fa-twitter"></i><a href="#" class="social-link">twitter</a></li>
+                    <li><i class="fab fa-facebook"></i><a href="#" class="social-link">facebook</a></li>
+                    <li><i class="fab fa-instagram"></i><a href="#" class="social-link">instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
